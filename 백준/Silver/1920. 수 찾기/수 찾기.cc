dcec6867bfeb8f38da6d00b26cc2e86e2d730282
Silbet <65993038+Silbet@users.noreply.github.com>
2026-01-21 10:22:36 +0900

    [Silver IV] Title: 수 찾기, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,31 +1,50 @@
-// 1920. 수 찾기(해시맵 연습)
+// 1920. 수 찾기
 #include<iostream>
-#include<unordered_map>
+#include<algorithm>
 using namespace std;
+
+int A[100005];
+
+bool isExsist(int target, int n) {
+  int st = 0;
+  int en = n - 1;
+
+  while (st <= en) {
+    int mid = (st + en) / 2;
+
+    if (A[mid] > target) {
+      en = mid - 1;
+    }
+
+    if (A[mid] < target) {
+      st = mid + 1;
+    }
+
+    if (A[mid] == target) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
+
   int n;
   cin >> n;
-
-  unordered_map<int, bool> arr;
   for (int i = 0; i < n; i++) {
-    int tmp;
-    cin >> tmp;
-    arr[tmp] = 1;
+    cin >> A[i];
   }
+  sort(A, A + n);
 
   int m;
   cin >> m;
   for (int i = 0; i < m; i++) {
-    int tmp;
-    cin >> tmp;
-    if (arr.count(tmp)) {
-      cout << "1\n";
-    }
-    else {
-      cout << "0\n";
-    }
+    int target;
+    cin >> target;
+
+    cout << isExsist(target, n) << "\n";
   }
 }
