44568c4588cc904f91da80ef684b3221378d445a
Silbet <65993038+Silbet@users.noreply.github.com>
2026-02-05 16:53:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 260 ms, Memory: 80144 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,68 +1,30 @@
-// 10816. 숫자 카드 2
+// 10816. 숫자카드(복습)
 #include<iostream>
+#include<string>
 #include<algorithm>
 using namespace std;
 
-int card[500005];
-
-
-int lowerBoundBinarySearch(int target, int n) {
-  int st = 0;
-  int en = n - 1;
-
-  while (st <= en) {
-    int mid = (st + en) / 2;
-
-    if (card[mid] >= target) {
-      en = mid - 1;
-    }
-
-    if (card[mid] < target) {
-      st = mid + 1;
-    }
-  }
-
-  if (card[st] == target) return st;
-  return 0;
-}
-
-int upperBoundBinarySearch(int target, int n) {
-  int st = 0;
-  int en = n - 1;
-
-  while (st <= en) {
-    int mid = (st + en) / 2;
-
-    if (card[mid] > target) {
-      en = mid - 1;
-    }
-
-    if (card[mid] <= target) {
-      st = mid + 1;
-    }
-  }
-
-  if (card[en] == target) return en + 1;
-  return 0;
-}
+int number[20000005];
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
+  
   int n;
   cin >> n;
+
   for (int i = 0; i < n; i++) {
-    cin >> card[i];
+    int tmp;
+    cin >> tmp;
+    number[tmp + 10000000]++;
   }
-  sort(card, card + n);
 
   int m;
   cin >> m;
   for (int i = 0; i < m; i++) {
-    int target;
-    cin >> target;
-
-    cout << upperBoundBinarySearch(target, n) - lowerBoundBinarySearch(target, n) << " ";
+    int tmp;
+    cin >> tmp;
+    cout << number[tmp + 10000000] << " ";
   }
+
 }
