00daa1f7ab69f4533e4f28b9dff24b05c332ddd9
Silbet <65993038+Silbet@users.noreply.github.com>
2026-02-12 08:26:56 +0900

    [Gold V] Title: 다각형의 면적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,32 +1,12 @@
-// 2166. 다각형의 면적
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-using namespace std;
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int n;
-  cin >> n;
-
-  long long x1, y1, x2, y2, x, y;
-  cin >> x2 >> y2;
-  x = x2;
-  y = y2;
-
-  long long result = 0;
-  for (int i = 0; i < n - 1; i++) {
-    x1 = x2;
-    y1 = y2;
-    cin >> x2 >> y2;
-
-    result += (x1 * y2 - x2 * y1);
-  }
-  result += (x2 * y - x * y2);
-
-  double width = abs(result) * 0.5;
-
-  printf("%.1lf", width);
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
